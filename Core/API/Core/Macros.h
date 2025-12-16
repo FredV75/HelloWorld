25ddde3a2303257d47737190278430adcc99bd20
Frederic Vallieres <Frederic.Vallieres@videotron.ca>
2025-12-16 18:22:19 -0500

    FVL_PRAGMA_WARNING

--- Core/API/Core/Macros.h
+++ Core/API/Core/Macros.h
@@ -5,6 +5,9 @@
 #define FVL_TOSTRING(x) FVL_STRINGIFY(x)
 
 #ifdef __linux__
+#define FVL_MSVC_PRAGMA_WARNING_PUSH
+#define FVL_MSVC_PRAGMA_WARNING(x)
+#define FVL_MSVC_PRAGMA_WARNING_POP
 #define FVL_GCC_PRAGMA_WARNING_PUSH _Pragma("GCC diagnostic push")
 #define FVL_GCC_PRAGMA_WARNING(x) _Pragma(FVL_TOSTRING(GCC diagnostic ignored x))
 #define FVL_GCC_PRAGMA_WARNING_POP _Pragma("GCC diagnostic pop")
@@ -12,6 +15,9 @@
 #define FVL_MSVC_PRAGMA_WARNING_PUSH __pragma(warning(push))
 #define FVL_MSVC_PRAGMA_WARNING(x) __pragma(warning(disable: x))
 #define FVL_MSVC_PRAGMA_WARNING_POP __pragma(warning(pop))
+#define FVL_GCC_PRAGMA_WARNING_PUSH
+#define FVL_GCC_PRAGMA_WARNING(x)
+#define FVL_GCC_PRAGMA_WARNING_POP
 #endif
 
 #define FVL_VIRTUAL_DESTRUCTOR(name)\
