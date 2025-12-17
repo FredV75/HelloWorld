27ee9c5e4a8a2908836699fe1404e1481aad619a
Frederic Vallieres <Frederic.Vallieres@videotron.ca>
2025-12-16 19:06:33 -0500

    FVL_TYPEDEF

--- Core/API/Core/Macros.h
+++ Core/API/Core/Macros.h
@@ -9,27 +9,30 @@
 #define FVL_TYPEDEF typedef void FVL_CONCAT(Line, __LINE__)
 
 #ifdef __linux__
-#define FVL_MSVC_PRAGMA_WARNING_PUSH FVL_TYPEDEF
-#define FVL_MSVC_PRAGMA_WARNING(x) FVL_TYPEDEF
-#define FVL_MSVC_PRAGMA_WARNING_POP FVL_TYPEDEF
-#define FVL_GCC_PRAGMA_WARNING_PUSH _Pragma("GCC diagnostic push"); FVL_TYPEDEF
-#define FVL_GCC_PRAGMA_WARNING(x) _Pragma(FVL_TOSTRING(GCC diagnostic ignored x)); FVL_TYPEDEF
-#define FVL_GCC_PRAGMA_WARNING_POP _Pragma("GCC diagnostic pop"); FVL_TYPEDEF
+#define FVL_MSVC_PRAGMA_WARNING_PUSH
+#define FVL_MSVC_PRAGMA_WARNING(x)
+#define FVL_MSVC_PRAGMA_WARNING_POP
+#define FVL_GCC_PRAGMA_WARNING_PUSH _Pragma("GCC diagnostic push")
+#define FVL_GCC_PRAGMA_WARNING(x) _Pragma(FVL_TOSTRING(GCC diagnostic ignored x))
+#define FVL_GCC_PRAGMA_WARNING_POP _Pragma("GCC diagnostic pop")
 #else
-#define FVL_MSVC_PRAGMA_WARNING_PUSH __pragma(warning(push)); FVL_TYPEDEF
-#define FVL_MSVC_PRAGMA_WARNING(x) __pragma(warning(disable: x)); FVL_TYPEDEF
-#define FVL_MSVC_PRAGMA_WARNING_POP __pragma(warning(pop)); FVL_TYPEDEF
-#define FVL_GCC_PRAGMA_WARNING_PUSH FVL_TYPEDEF
-#define FVL_GCC_PRAGMA_WARNING(x) FVL_TYPEDEF
-#define FVL_GCC_PRAGMA_WARNING_POP FVL_TYPEDEF
+#define FVL_MSVC_PRAGMA_WARNING_PUSH __pragma(warning(push))
+#define FVL_MSVC_PRAGMA_WARNING(x) __pragma(warning(disable: x))
+#define FVL_MSVC_PRAGMA_WARNING_POP __pragma(warning(pop))
+#define FVL_GCC_PRAGMA_WARNING_PUSH
+#define FVL_GCC_PRAGMA_WARNING(x)
+#define FVL_GCC_PRAGMA_WARNING_POP
 #endif
 
 #define FVL_VIRTUAL_DESTRUCTOR(name)\
-    FVL_MSVC_PRAGMA_WARNING_PUSH;\
-    FVL_MSVC_PRAGMA_WARNING(4710);\
+    FVL_GCC_PRAGMA_WARNING_PUSH\
+    FVL_GCC_PRAGMA_WARNING(pedantic)\
+    FVL_MSVC_PRAGMA_WARNING_PUSH\
+    FVL_MSVC_PRAGMA_WARNING(4710)\
     name() = default;\
     virtual ~name() = default;\
     name(const name &) = default;\
     name &operator=(const name &) = default;\
-    FVL_MSVC_PRAGMA_WARNING_POP
+    FVL_MSVC_PRAGMA_WARNING_POP\
+    FVL_GCC_PRAGMA_WARNING_POP
 
